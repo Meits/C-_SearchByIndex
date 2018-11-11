d7c991d0f35cd7ba2056dd7d0ca18e3b4bcde0df
meitslin <meitslin@gmail.com>
2018-11-11 10:21:05 +0200

    init

---
+++ ConsoleApp7/XmlReader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp7
+{
+    class XmlReader
+    {
+        XmlDocument xDoc;
+
+        public String searchByIndex(string str)
+        {
+            string result = "";
+
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.FirstChild.Name == "Index")
+                {
+                    XmlNode attr = null;
+                    try
+                    {
+                        attr = xnode.FirstChild.Attributes.GetNamedItem("value");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    if (attr != null)
+                    {
+                        if (attr.Value == str)
+                        {
+                            foreach (XmlNode childnode in xnode.ChildNodes)
+                            {
+                                if (childnode.Name != "Index")
+                                {
+                                    if (childnode.Name == "Street")
+                                    {
+                                        result += childnode.Attributes.GetNamedItem("br").Value + Environment.NewLine;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            if(result == "")
+            {
+                result = "НЕ найдено";
+            }
+
+            return result;
+        }
+        public XmlReader()
+        {
+           
+            xDoc = new XmlDocument();
+            xDoc.Load("../../XMLFile1.xml");
+
+        
+        }
+    }
+}
---
+++ ConsoleApp7/bin/Debug/XmlReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp7
+{
+    class XmlReader
+    {
+        XmlDocument xDoc;
+        public XmlReader()
+        {
+            xDoc = new XmlDocument();
+            xDoc.Load("../../XMLFile1.xml");
+
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    if (attr != null)
+                        Console.WriteLine(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    // если узел - company
+                    if (childnode.Name == "company")
+                    {
+                        Console.WriteLine("Компания: {0}", childnode.InnerText);
+                    }
+                    // если узел age
+                    if (childnode.Name == "age")
+                    {
+                        Console.WriteLine("Возраст: {0}", childnode.InnerText);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
